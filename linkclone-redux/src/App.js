709ddfe6d41a451786a352050917b3744a3721f8
Aryan <aryansanjaybollam124@gmail.com>
2025-04-29 19:43:01 +0530

    Fix: apply updated .gitignore and clean tracked files

---
+++ linkclone-redux/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Header from './header';
+
+
+function App() {
+  return (
+    <div className="app">
+      <h1>This is start </h1>
+      //header;
+      <Header />
+      //sidebar
+      //feed
+      //trending
+    </div>
+  );
+}
+
+export default App;
